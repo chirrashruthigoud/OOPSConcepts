0c642c9428ce7e7cde93af3a0dd54ef8532e0df4
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-04-22 12:42:13 +0530

    Add : create class

---
+++ OOPSConcept/OOPSConcept/ClassName.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSConcept
+{
+    internal class ClassName
+    {
+        public void PrintName()
+        {
+                Console.WriteLine("Shruthi");
+        }
+    }
+}
+
---
+++ OOPSConcept/OOPSConcept/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPSConcept
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassName name = new ClassName();
+            name.PrintName();
+            Console.ReadKey();
+        }
+    }
+}
